42b333aa2c3c90c19052ecfcdc738cfb54ab73dc
Wilkin Novo <wnovo@me.com>
2019-02-27 16:28:41 -0500

    video context...

--- public/contextMenu.js
+++ public/contextMenu.js
@@ -1,7 +1,13 @@
 /*  -------------------- © 2019. NOVO WORK SYSTEMS | http://novo.ws ------------------- */
 /* -------------------------- Use @ your own risk. -------------------------- */
-let contextMenuPoster = {
-  id: 'NovoCast',
+const contextMenuVideo = {
+  id: 'video',
+  title: 'Cast to Roku',
+  contexts: ['video'],
+  targetUrlPatterns: ['*://*/*.mp4*', '*://*/*.webm*']
+};
+const contextMenuPoster = {
+  id: 'image',
   title: 'Cast to Roku',
   contexts: ['image'],
   targetUrlPatterns: [
@@ -14,22 +20,64 @@ let contextMenuPoster = {
 };
 
 chrome.contextMenus.create(contextMenuPoster);
-chrome.contextMenus.onClicked.addListener(obj => {
+chrome.contextMenus.create(contextMenuVideo);
+chrome.contextMenus.onClicked.addListener((obj, tab) => {
   // NOTE This will probably be extracted out into a reusable function.
-  const url = `http://${localStorage.getItem(
-    'ip'
-  )}:8060/launch/dev?poster=${encodeURIComponent(obj.srcUrl)}`;
+  let srcURL = '';
+  let mediaType = '';
+  let title = '';
+  let subtitle = '';
+  const url = `http://${localStorage.getItem('ip')}:8060/launch/dev?`;
+  switch (obj.mediaType) {
+    case 'video':
+      mediaType = 'url';
+      srcURL = obj.srcUrl;
+      title = tab.title;
+      subtitle = 'iyy2u323yu2yu32y3uy2u3';
+      console.log(tab);
+      // Make the request via fetch
+      fetch(
+        url +
+          `${mediaType}=${encodeURIComponent(
+            srcURL
+          )}&title=${encodeURIComponent(title)}&subtitle=${encodeURIComponent(
+            subtitle
+          )}`,
+        {
+          method: 'POST'
+        }
+      )
+        .then(re => {
+          console.log(re);
+        })
+        .catch(e => {
+          if (e.toString() === 'TypeError: Failed to fetch') {
+            alert('Wrong IP Address.');
+          } else {
+            alert(e.toString());
+          }
+        });
+      break;
+    case 'image':
+      mediaType = 'poster';
+      srcURL = obj.srcUrl;
+      // Make the request via fetch
+      fetch(url + `${mediaType}=${encodeURIComponent(srcURL)}`, {
+        method: 'POST'
+      })
+        .then(re => {
+          console.log(re);
+        })
+        .catch(e => {
+          if (e.toString() === 'TypeError: Failed to fetch') {
+            alert('Wrong IP Address.');
+          } else {
+            alert(e.toString());
+          }
+        });
+      break;
 
-  // Make the request via fetch
-  fetch(url, { method: 'POST' })
-    .then(re => {
-      console.log(re);
-    })
-    .catch(e => {
-      if (e.toString() === 'TypeError: Failed to fetch') {
-        alert('Wrong IP Address.');
-      } else {
-        alert(e.toString());
-      }
-    });
+    default:
+      break;
+  }
 });
