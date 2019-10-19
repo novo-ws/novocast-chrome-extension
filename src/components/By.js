5cd05da515cbe7fd677b05122a839c36fecf504e
Wilkin Novo <w@novo.ws>
2019-10-19 05:38:07 -0400

    name change +yarn

--- src/components/By.js
+++ src/components/By.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const By = () => {
-  const URL = 'http://www.novo.ws';
-  const LOGO = 'Novo Work Systems';
+  const URL = "http://www.novo.ws";
+  const LOGO = "Novo";
   return (
     <div className="text-muted">
-      <p align="center" style={{ paddingTop: '60px', fontSize: '12px' }}>
+      <p align="center" style={{ paddingTop: "60px", fontSize: "12px" }}>
         <i className="fas fa-circle fa-2x" />
         <br />
-        Created by{' '}
-        <a href={URL} style={{ color: '#777' }} target="_blank">
+        Created by{" "}
+        <a href={URL} style={{ color: "#777" }} target="_blank">
           {LOGO}
         </a>
       </p>
